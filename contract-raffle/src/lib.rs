57574df2793fb5043566eee4a2812a40d2ab5cc3
Deveus <raffree.dev@gmail.com>
2021-12-22 00:06:01 +0200

    minor fixes

--- contract-raffle/src/lib.rs
+++ contract-raffle/src/lib.rs
@@ -9,7 +9,7 @@ use near_sdk::collections::UnorderedMap;
 use near_sdk::json_types::{ValidAccountId, U128};
 use near_sdk::PromiseOrValue;
 use near_sdk::{env, log, near_bindgen, AccountId, Balance, BorshStorageKey, PanicOnDefault};
-use std::convert::{AsRef, From, TryFrom};
+use std::convert::{AsRef, From};
 
 use internal::*;
 
@@ -24,7 +24,6 @@ enum RaffleInstruction {
     Unknown,
     BuyPrize,
     BuyTicket,
-
 }
 
 impl From<String> for RaffleInstruction {
@@ -32,7 +31,6 @@ impl From<String> for RaffleInstruction {
         match &item[..] {
             "buy_ticket" => RaffleInstruction::BuyTicket,
             "buy_prize" => RaffleInstruction::BuyPrize,
-
             _ => RaffleInstruction::Unknown,
         }
     }
@@ -45,6 +43,33 @@ pub struct RaffleContract {
     ticket: RaffleTicket,
     fungible_token_account_id: AccountId,
 }
+
+
+#[near_bindgen]
+impl RaffleContract {
+    #[init]
+    pub fn new(
+        fungible_token_account_id: AccountId,
+        tokens_per_ticket: Balance,
+        number_of_predefined: i16,
+    ) -> Self {
+        assert_initialized();
+        Self {
+            ticket: RaffleTicket::new(tokens_per_ticket, number_of_predefined),
+            fungible_token_account_id,
+        }
+    }
+
+
+    pub fn total_tickets(&self)->u64{
+      self.ticket.total_available()
+    }
+    
+    pub fn reset(&mut self) {
+        self.ticket.reset();
+    }
+}
+
 
 #[near_bindgen]
 impl FungibleTokenReceiver for RaffleContract {
@@ -84,30 +109,5 @@ impl FungibleTokenReceiver for RaffleContract {
                 PromiseOrValue::Value(amount)
             }
         }
-    }
-}
-
-#[near_bindgen]
-impl RaffleContract {
-    #[init]
-    pub fn new(
-        fungible_token_account_id: AccountId,
-        tokens_per_ticket: Balance,
-        number_of_predefined: i16,
-    ) -> Self {
-        assert_initialized();
-        Self {
-            ticket: RaffleTicket::new(tokens_per_ticket, number_of_predefined),
-            fungible_token_account_id,
-        }
-    }
-
-
-    pub fn total_tickets(&self)->u64{
-      self.ticket.total_available()
-    }
-    
-    pub fn reset(&mut self) {
-        self.ticket.reset();
     }
 }
